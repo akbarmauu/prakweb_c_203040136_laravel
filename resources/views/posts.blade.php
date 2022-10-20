8b5f5397584aeca57e43847bf1c53c9c6f931460
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2022-10-21 00:48:32 +0700

    Menyelesaikan Video-3 (Masih Error)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WPU Blog | Posts</title>
-</head>
-<body>
-    <h1>Halaman Posts</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+   
+    @foreach ($posts as $posts)
+    <article class="mb-5">
+        <h2>
+            <a href="/posts/{{ $posts["title"] }}">{{ $posts["title"] }}</a>
+        </h2>
+        <h5>By : {{ $posts["author"] }}</h5>
+        <p>{{ $posts["body"] }}</p>
+    </article>
+    @endforeach
+
+@endsection
+
