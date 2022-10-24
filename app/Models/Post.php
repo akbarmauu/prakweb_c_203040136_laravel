7bf39eb14bd4cb83190809cdb305c5587b938045
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2022-10-25 00:18:28 +0700

    Menyelesaikan Video ke-7

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,14 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = 
-    [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Akbar Maulana",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam ex saepe dignissimos dolorum, neque, repudiandae laudantium odio aut omnis facere corporis molestiae doloribus praesentium quam rem magnam suscipit quaerat magni! Iste odit molestias recusandae tempora cum beatae aperiam architecto reprehenderit illum rerum commodi laboriosam, consectetur, magni aliquid, officiis distinctio exercitationem! Voluptate iste esse est adipisci exercitationem minus? Quibusdam laborum quidem eos, eius, accusantium, exercitationem totam minus nemo pariatur tempore quia enim omnis nisi sequi repellat! Consequatur, officia. Rem, inventore suscipit."
-        ],
-        [
-            "title" => "Judul Post Akbar",
-            "slug" => "judul-post-kedua",
-            "author" => "Akbar Maulana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatum a dolorum totam. Ex eligendi voluptatem consequatur iste rerum, quasi tempore fuga illum in necessitatibus, sapiente laboriosam? Adipisci voluptatem, atque veritatis enim numquam, aut placeat maiores omnis aperiam delectus sint? Voluptas commodi, quisquam perspiciatis ducimus reprehenderit rerum repellendus fuga quibusdam maxime, quis aperiam magnam odit earum eaque porro voluptatum illo? Voluptatum debitis optio nihil atque dolore sapiente molestias ab, eius corporis numquam enim earum sed autem accusamus dolores asperiores repellendus accusantium et ducimus. Sit vel placeat laborum. Consequuntur, reprehenderit. Consectetur distinctio, quibusdam omnis ipsa dolorem soluta possimus. Officiis, aliquid labore."
-        ],
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect (self::$blog_posts);
-    }
 
-    public static function find($slug)
-    {
-        $posts = static::all();
-        // $post = [];
-        // foreach($posts as $p) {
-        //      if($p["slug"] === $slug) {
-        //            $post = $p;
-        //   }          
-        // }
-
-        return $posts->firstWhere('slug', $slug);
-    }
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
