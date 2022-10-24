a8021b6de21a12c4adfdb6c0ce7dd3ccce67607e
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2022-10-24 11:32:51 +0700

    Menyelesaikan Video-5 model,Collection & controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -31,54 +34,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Akbar Maulana",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam ex saepe dignissimos dolorum, neque, repudiandae laudantium odio aut omnis facere corporis molestiae doloribus praesentium quam rem magnam suscipit quaerat magni! Iste odit molestias recusandae tempora cum beatae aperiam architecto reprehenderit illum rerum commodi laboriosam, consectetur, magni aliquid, officiis distinctio exercitationem! Voluptate iste esse est adipisci exercitationem minus? Quibusdam laborum quidem eos, eius, accusantium, exercitationem totam minus nemo pariatur tempore quia enim omnis nisi sequi repellat! Consequatur, officia. Rem, inventore suscipit."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Akbar Maulana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatum a dolorum totam. Ex eligendi voluptatem consequatur iste rerum, quasi tempore fuga illum in necessitatibus, sapiente laboriosam? Adipisci voluptatem, atque veritatis enim numquam, aut placeat maiores omnis aperiam delectus sint? Voluptas commodi, quisquam perspiciatis ducimus reprehenderit rerum repellendus fuga quibusdam maxime, quis aperiam magnam odit earum eaque porro voluptatum illo? Voluptatum debitis optio nihil atque dolore sapiente molestias ab, eius corporis numquam enim earum sed autem accusamus dolores asperiores repellendus accusantium et ducimus. Sit vel placeat laborum. Consequuntur, reprehenderit. Consectetur distinctio, quibusdam omnis ipsa dolorem soluta possimus. Officiis, aliquid labore."
-        ],
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-//halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Akbar Maulana",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam ex saepe dignissimos dolorum, neque, repudiandae laudantium odio aut omnis facere corporis molestiae doloribus praesentium quam rem magnam suscipit quaerat magni! Iste odit molestias recusandae tempora cum beatae aperiam architecto reprehenderit illum rerum commodi laboriosam, consectetur, magni aliquid, officiis distinctio exercitationem! Voluptate iste esse est adipisci exercitationem minus? Quibusdam laborum quidem eos, eius, accusantium, exercitationem totam minus nemo pariatur tempore quia enim omnis nisi sequi repellat! Consequatur, officia. Rem, inventore suscipit."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Akbar Maulana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptatum a dolorum totam. Ex eligendi voluptatem consequatur iste rerum, quasi tempore fuga illum in necessitatibus, sapiente laboriosam? Adipisci voluptatem, atque veritatis enim numquam, aut placeat maiores omnis aperiam delectus sint? Voluptas commodi, quisquam perspiciatis ducimus reprehenderit rerum repellendus fuga quibusdam maxime, quis aperiam magnam odit earum eaque porro voluptatum illo? Voluptatum debitis optio nihil atque dolore sapiente molestias ab, eius corporis numquam enim earum sed autem accusamus dolores asperiores repellendus accusantium et ducimus. Sit vel placeat laborum. Consequuntur, reprehenderit. Consectetur distinctio, quibusdam omnis ipsa dolorem soluta possimus. Officiis, aliquid labore."
-        ],
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-    if($post["slug"] === $slug) {
-         $new_post = $post;
-     }
-}
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
