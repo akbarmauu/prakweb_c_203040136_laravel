eb81c65d3d5db88edc6afe7cce501567ca8f44c2
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2022-10-26 02:02:52 +0700

    Menyelesaikan Video 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,76 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Akbar Maulana',
+            'email' => 'akbarmaumau12@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+        User::create([
+            'name' => 'Farhat Aljaelani',
+            'email' => 'Farhatalajealani@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
+            sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
+            Illo modi architecto voluptatibus ab voluptatem ipsa
+            ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
+            sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
+            Illo modi architecto voluptatibus ab voluptatem ipsa
+            ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
+            sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
+            Illo modi architecto voluptatibus ab voluptatem ipsa
+            ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
+            sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
+            Illo modi architecto voluptatibus ab voluptatem ipsa
+            ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+        
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('container')
   <article>
     <h2>{{ $post->title }}</h2>
-    <p>By. Akbar Maulana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
   {!! $post->body !!}
   </article>
 
-  <a href="/blog">Back to Posts</a>
+  <a href="/blog" class="text-decoration-none d-block mt-4">Back to Posts</a>
 @endsection
