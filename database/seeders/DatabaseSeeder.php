c0503d9b62bae4ccb185b21c446225a45fff53c7
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2022-10-26 11:56:51 +0700

    Menyelesaikan Video-10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,23 +17,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            'name' => 'Akbar Maulana',
-            'email' => 'akbarmaumau12@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
-        User::create([
-            'name' => 'Farhat Aljaelani',
-            'email' => 'Farhatalajealani@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Akbar Maulana',
+        //     'email' => 'akbarmaumau12@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+        // User::create([
+        //     'name' => 'Farhat Aljaelani',
+        //     'email' => 'Farhatalajealani@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -45,54 +47,56 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
+        Post::factory(15)->create();
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
-            sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
-            Illo modi architecto voluptatibus ab voluptatem ipsa
-            ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
-            sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
-            Illo modi architecto voluptatibus ab voluptatem ipsa
-            ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
+        //     sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
+        //     Illo modi architecto voluptatibus ab voluptatem ipsa
+        //     ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
-            sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
-            Illo modi architecto voluptatibus ab voluptatem ipsa
-            ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
+        //     sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
+        //     Illo modi architecto voluptatibus ab voluptatem ipsa
+        //     ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
-            sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
-            Illo modi architecto voluptatibus ab voluptatem ipsa
-            ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
+        //     sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
+        //     Illo modi architecto voluptatibus ab voluptatem ipsa
+        //     ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde velit labore. Ratione odit laborum commodi eius quasi,
+        //     sint veniam atque, provident ipsam eos molestias delectus a saepe, facere iusto quo ducimus vitae quas recusandae distinctio aut. 
+        //     Illo modi architecto voluptatibus ab voluptatem ipsa
+        //     ratione dolorem quas mollitia ut, maiores nulla! Saepe ipsum praesentium ullam a, aspernatur corrupti adipisci reiciendis quis non optio maxime eum vitae? Sequi, voluptatum? Facilis,',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
         
     }
 }
