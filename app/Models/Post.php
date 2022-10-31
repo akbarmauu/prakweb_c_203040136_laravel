0baa61db4ecdf856e7bfeec9ba06816002a6dbbf
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2022-10-31 23:32:44 +0700

    Menyelesaikan Video 13 (Search Masih Erorr)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,21 @@ class Post extends Model
     protected $guarded = ['id'];
     protected $with = ['category', 'author'];
 
+    public function scopeFillter($query, array $fillters)
+    {
+
+        $query->when($fillters['search'] ?? false, function($query, $search){
+            return $query->where('title', 'like', '%' . $search('search') . '%')
+            ->orWhere('body', 'like', '%' . $search('search') . '%');
+        }) ;       
+
+        $query->when($fillters['category'] ?? false, function($query, $category){
+            return $query->whereHas('category', function($query) use ($category){
+                    $query->where('slug', $category);
+            });
+        });
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home',[
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about',[
         "title" => "About",
+        "active" => 'about',
         "name" => "Akbar Maulana",
         "email" => "akbarmaumau12@gmail.com",
         "image" => "profil.jpeg"
