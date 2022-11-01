39bff66c3220ab9ee38d347e2ed73011039a2eef
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2022-11-01 19:10:17 +0700

    Menyelesaikan Video 13

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,26 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $title = '';
+
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
+        }
+
         return view('posts', [
-            "title" => "All Posts",
-            "active" => 'posts',
-            "posts" =>  Post::latest()->fillter(request(['search', 'category']))->get()
+            "title" => "All Posts" . $title,
+            "active" => "posts",
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString(),
         ]);
     }
-
     public function show(Post $post)
     {
         return view('post', [
             "title" => "Single Post",
-            "active" => 'posts',
-            "post" => $post
+            "active" => "posts",
+            "post" => $post,
         ]);
     }
 }
