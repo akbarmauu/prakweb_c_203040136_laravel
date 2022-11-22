959ecb802498c93b58881a2c17316a6c7aee7bad
akbarmauu <76439418+akbarmauu@users.noreply.github.com>
2022-11-22 12:08:47 +0700

    Menyelesaikan Video ke-19

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -14,7 +14,6 @@
          <article class="my-3 fs-5">
              {!! $post->body !!}
          </article>
-    <a href="/blog" class="text-decoration-none d-block mt-4">Back to Posts</a>
       </div>
     </div>  
   </div>
